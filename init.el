b24de60ddc528a3f43c9893aa7e708dfb4d44dd8
lampui <lpwowo@gmail.com>
2019-02-19 18:35:24 +0800

    use helpful & org-publish-project-alist

--- init.el
+++ init.el
@@ -106,3 +106,22 @@
       (t 0))
 ;; 配置 emacsclient -c 启动下的字体
 ;; (setq default-frame-alist '((font . "Fira Mono-11")))
+;(define-globalized-minor-mode
+;  global-text-scale-mode
+;  text-scale-mode
+;  (lambda () (text-scale-mode 1)))
+;
+;(defun global-text-scale-adjust (inc) (interactive)
+;       (text-scale-set 1)
+;       (kill-local-variable 'text-scale-mode-amount)
+;       (setq-default text-scale-mode-amount (+ text-scale-mode-amount inc))
+;       (global-text-scale-mode 1)
+;       )
+;(global-set-key (kbd "C-x C-0")
+;                '(lambda () (interactive)
+;                   (global-text-scale-adjust (- text-scale-mode-amount))
+;                   (global-text-scale-mode -1)))
+;(global-set-key (kbd "C-x C-+")
+;                '(lambda () (interactive) (global-text-scale-adjust 1)))
+;(global-set-key (kbd "C-x C--")
+;                '(lambda () (interactive) (global-text-scale-adjust -1)))
