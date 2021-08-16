2117750230b59126f5d53f1d8c9028c557f53749
Laam Pui <lpwowo@gmail.com>
2021-08-16 22:46:35 +0800

    feat: add git-gutter

--- init.el
+++ init.el
@@ -46,3 +46,7 @@
                            charset
                            (font-spec :family "Noto Serif CJK SC Medium"))) )
       (t 0))
+(setq url-proxy-services
+   '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+     ("http" . "127.0.0.1:7890")
+     ("https" . "127.0.0.1:7890")))
